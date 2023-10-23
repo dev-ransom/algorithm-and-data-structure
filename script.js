2c027c48574f5faae023dfd42d2096301254e47e
idungstanley <sundaystanley56@gmail.com>
2023-10-23 10:38:59 +0100

    added code challenge for power of three

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function isPowerOfTwo(n){
-    if(n <= 0) return false;
-    return (n & (n - 1))=== 0;
+function isPowerOfThree(n){
+    if(n <= 0) return false; //checks if the number is less than 0 or zero
+    while(n > 1){
+        if(n % 3 !== 0){
+            return false; // 'n' is not divisible by 3
+        }
+        n /= 3;
+        console.log(n);
+    }
+    return true;  //checks if number is one
+
 }
 
-console.log(isPowerOfTwo(4));
+console.log(isPowerOfThree(20));
